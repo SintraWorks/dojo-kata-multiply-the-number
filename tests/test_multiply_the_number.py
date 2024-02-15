e602f0692da577b0ffebf4bc866a5c39bd30e9bf
Antonio Nunes <antonio.nunes@sintraworks.com>
2024-02-15 16:08:47 +0100

    Correction to the test validation text

--- tests/test_multiply_the_number.py
+++ tests/test_multiply_the_number.py
@@ -3,5 +3,5 @@ from modules import multiply_the_number
 
 def describe_multiply_the_number():
     def validate_that_input_3_returns_15():
-        """🧪 expect the validation to be true"""
+        """🧪 expect input 3 to return 15"""
         assert multiply_the_number.multiply(3) == 15
