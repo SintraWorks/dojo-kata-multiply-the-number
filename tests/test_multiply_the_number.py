2aea56bdd5178b8a90cad8882b3b9ea3907ae014
Antonio Nunes <antonio.nunes@sintraworks.com>
2024-02-15 16:23:07 +0100

    Pass test 3
    No refactor needed

--- tests/test_multiply_the_number.py
+++ tests/test_multiply_the_number.py
@@ -9,3 +9,7 @@ def describe_multiply_the_number():
     def validate_that_input_10_returns_250():
         """🧪 expect input 10 to return 250"""
         assert multiply_the_number.multiply(10) == 250
+
+    def validate_that_input_200_returns_25000():
+        """🧪 expect input 200 to return 25000"""
+        assert multiply_the_number.multiply(200) == 25000
